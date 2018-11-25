075c5185dac2981ca8454054b71e6dcce125a67d
jay_liao <chungyojay0206@gmail.com>
2018-11-25 22:29:08 +0800

    [Changed]區塊鏈

--- block.go
+++ block.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
-	"log"
 	"strconv"
 	"time"
 )
@@ -18,16 +17,12 @@ type Block struct {
 func (b *Block) SetHash() {
 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	log.Printf("headers ---> %+v \n", headers)
 	hash := sha256.Sum256(headers)
-	log.Printf("hash ---> %v \n", hash)
 	b.Hash = hash[:]
-	log.Printf("b.Hash ---> %x \n", b.Hash)
 
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	log.Printf("data to byte ---> %+v \n", []byte(data))
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
 	block.SetHash()
 	return block
