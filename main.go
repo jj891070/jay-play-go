7c3fd16443e1dccc4b9fca6c3b6b24cb982f0fd2
jay_liao <chungyojay0206@gmail.com>
2019-03-26 11:50:21 +0800

    [Added]gcp-telegram-notify

--- main.go
+++ main.go
@@ -1,54 +1,133 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 	"time"
 )
 
-var way map[int]string
+func main() {
 
-func benchmarkStringFunction(n int, index int) (d time.Duration) {
-	v := "ni shuo wo shi bu shi tai wu liao le a?"
-	var s string
-	var buf bytes.Buffer
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8096", nil)
 
-	t0 := time.Now()
-	for i := 0; i < n; i++ {
-		switch index {
-		case 0: // fmt.Sprintf
-			s = fmt.Sprintf("%s[%s]", s, v)
-		case 1: // string +
-			s = s + "[" + v + "]"
-		case 2: // strings.Join
-			s = strings.Join([]string{s, "[", v, "]"}, "")
-		case 3: // stable bytes.Buffer
-			buf.WriteString("[")
-			buf.WriteString(v)
-			buf.WriteString("]")
-		}
-
-	}
-	d = time.Since(t0)
-	if index == 3 {
-		s = buf.String()
-	}
-	fmt.Printf("string len: %d\t", len(s))
-	fmt.Printf("time of [%s]=\t %v\n", way[index], d)
-	return d
 }
 
-func main() {
-	way = make(map[int]string, 5)
-	way[0] = "fmt.Sprintf"
-	way[1] = "+"
-	way[2] = "strings.Join"
-	way[3] = "bytes.Buffer"
+type GcpData struct {
+	Incident struct {
+		IncidentID    string `json:"incident_id"`
+		ResourceID    string `json:"resource_id"`
+		ResourceName  string `json:"resource_name"`
+		State         string `json:"state"`
+		StartedAt     *int64 `json:"started_at"`
+		EndedAt       *int64 `json:"ended_at"`
+		PolicyName    string `json:"policy_name"`
+		ConditionName string `json:"condition_name"`
+		URL           string `json:"url"`
+		Summary       string `json:"summary"`
+	} `json:"incident"`
+}
 
-	k := 4
-	d := [5]time.Duration{}
-	for i := 0; i < k; i++ {
-		d[i] = benchmarkStringFunction(10000, i)
+type Notify struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+// HelloWorld prints the JSON encoded "message" field in the body
+// of the request or "Hello, World!" if there isn't one.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	var (
+		input  *GcpData
+		output Notify
+		err    error
+	)
+
+	url := "https://api.telegram.org/bot725691005:AAFGVkxe-mdDrLrlpeyPNG5c3dqjtD0dhPg/sendMessage"
+	output.ChatID = -300312399
+
+	session := r.Header.Get("Authorization")
+	if session != "Basic aW5zbG90OnF3ZTEyMw==" {
+		return
 	}
+
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	if err = json.Unmarshal(buf, &input); err != nil {
+		log.Printf("😈  Json Decode Error ---> %+v \n", err)
+		output.Text = string(buf)
+		SendMessage("POST", url, output)
+		return
+	}
+
+	if input == nil {
+		log.Printf("😈  輸入資料為null \n")
+		return
+	}
+	var (
+		startAt int64
+		endAt   int64
+	)
+
+	if input.Incident.StartedAt != nil {
+		startAt = *input.Incident.StartedAt
+	}
+	if input.Incident.EndedAt != nil {
+		endAt = *input.Incident.StartedAt
+	}
+
+	output.Text = fmt.Sprintf(`
+	⭐  %s  ：  %s  ⭐
+
+	⏰  時間： %v  ～  %v
+
+	✍  網址： %v
+
+	`,
+		input.Incident.ConditionName, input.Incident.Summary,
+		time.Unix(startAt, 0).UTC().Add(time.Hour*8).Format("2006-01-02 15:04:05"), time.Unix(endAt, 0).UTC().Add(time.Hour*8).Format("2006-01-02 15:04:05"),
+		input.Incident.URL,
+	)
+
+	SendMessage("POST", url, output)
+
+}
+
+// SendMessage 發送訊息
+func SendMessage(method, url string, output Notify) {
+
+	outputData, err := json.Marshal(output)
+	if err != nil {
+		log.Printf("😈  Json Marshal Error ---> %v \n", err)
+		return
+	}
+
+	payload := strings.NewReader(string(outputData))
+
+	var (
+		req *http.Request
+	)
+	req, err = http.NewRequest("POST", url, payload)
+	if err != nil {
+		log.Printf("😈  NewRequest Error ---> %v \n", err)
+		return
+	}
+
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("authorization", "Basic aW5zbG90OnF3ZTEyMw==")
+	req.Header.Add("cache-control", "no-cache")
+
+	_, resErr := http.DefaultClient.Do(req)
+	if resErr != nil {
+		log.Printf("😈  Do Error ---> %v \n", err)
+		return
+	}
+
+	return
 }
