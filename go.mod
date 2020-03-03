a83cc407b537f0b7a3891551000ee7f6862df53a
jay_liao <chungyojay0206@gmail.com>
2020-03-03 20:15:40 +0800

    [Added]gin-stream

---
+++ go.mod
@@ -0,0 +1,9 @@
+module jay-play-go
+
+go 1.13
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.4.0
+	github.com/mattn/go-colorable v0.1.6
+)
--- main.go
+++ main.go
@@ -1,54 +1,36 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"strings"
+	"io"
+	"log"
 	"time"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/mattn/go-colorable"
 )
 
-var way map[int]string
-
-func benchmarkStringFunction(n int, index int) (d time.Duration) {
-	v := "ni shuo wo shi bu shi tai wu liao le a?"
-	var s string
-	var buf bytes.Buffer
-
-	t0 := time.Now()
-	for i := 0; i < n; i++ {
-		switch index {
-		case 0: // fmt.Sprintf
-			s = fmt.Sprintf("%s[%s]", s, v)
-		case 1: // string +
-			s = s + "[" + v + "]"
-		case 2: // strings.Join
-			s = strings.Join([]string{s, "[", v, "]"}, "")
-		case 3: // stable bytes.Buffer
-			buf.WriteString("[")
-			buf.WriteString(v)
-			buf.WriteString("]")
-		}
-
-	}
-	d = time.Since(t0)
-	if index == 3 {
-		s = buf.String()
-	}
-	fmt.Printf("string len: %d\t", len(s))
-	fmt.Printf("time of [%s]=\t %v\n", way[index], d)
-	return d
-}
-
 func main() {
-	way = make(map[int]string, 5)
-	way[0] = "fmt.Sprintf"
-	way[1] = "+"
-	way[2] = "strings.Join"
-	way[3] = "bytes.Buffer"
-
-	k := 4
-	d := [5]time.Duration{}
-	for i := 0; i < k; i++ {
-		d[i] = benchmarkStringFunction(10000, i)
-	}
+	gin.DefaultWriter = colorable.NewColorableStderr()
+	r := gin.Default()
+	r.GET("/stream", func(c *gin.Context) {
+		chanStream := make(chan int, 10)
+		go func() {
+			defer close(chanStream)
+			for i := 0; i < 5; i++ {
+				chanStream <- i
+				time.Sleep(time.Second * 1)
+			}
+		}()
+		c.Stream(func(w io.Writer) bool {
+			if msg, ok := <-chanStream; ok {
+				c.SSEvent("message", msg)
+				return true
+			}
+			log.Println("cancel")
+			return false
+		})
+	})
+	r.Use(static.Serve("/", static.LocalFile("./public", true)))
+	r.Run(":8087")
 }
