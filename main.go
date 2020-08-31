68ac9f085d2b19b58fa3c98708f088f38777f0c1
jay.liao <jay.liao@gosmio.biz>
2020-08-31 11:23:43 +0800

    [Tested]create file

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"os"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("💖 Hello World")
+
+	f,err := os.Create("jay.json")
+
+
+	if err !=nil {
+
+		fmt.Println( err.Error() )
+		f.Close()
+
+	} else {
+
+		_,err=f.Write([]byte(`{"name": "💖 Hello Jay"}`))
+		if err!= nil{
+			fmt.Println( err.Error() )
+		}
+
+	}
+
+	f.Close()
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello E.F.K")
