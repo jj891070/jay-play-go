43c481940b2e6e045296e16f20e296914e7fb775
廖聖傑 <jay@MacBook-Pro.local>
2021-03-13 12:16:51 +0800

    [Added]給明俊測試用給明俊測試用

--- golang/client/go.mod
+++ golang/client/go.mod
@@ -3,6 +3,9 @@ module gogrpc
 go 1.14
 
 require (
+	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/golang/protobuf v1.4.2
 	google.golang.org/grpc v1.35.0
-	google.golang.org/grpc/examples v0.0.0-20210211230339-9f3606cd0f76
+	google.golang.org/grpc/examples v0.0.0-20210211230339-9f3606cd0f76 // indirect
+	google.golang.org/protobuf v1.25.0
 )
--- golang/client/main.go
+++ golang/client/main.go
@@ -4,38 +4,57 @@ import (
 	"context"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	pb "gogrpc/protos"
 
+	"github.com/gofrs/uuid"
 	"google.golang.org/grpc"
 	// pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
-	address     = "localhost:50051"
-	defaultName = "jay"
+	address = "localhost:50051"
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	u2, err := uuid.NewV4()
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("failed to generate UUID: %v", err)
 	}
-	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+	var defaultName string = u2.String()
 
-	// Contact the server and print out its response.
-	name := defaultName
+	var interval int64 = 1000
 	if len(os.Args) > 1 {
-		name = os.Args[1]
+		n, err := strconv.ParseInt(os.Args[1], 10, 64)
+		if err == nil {
+			interval = n
+		}
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+
+	for {
+
+		// Set up a connection to the server.
+		conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		c := pb.NewGreeterClient(conn)
+
+		// Contact the server and print out its response.
+		name := defaultName
+		if len(os.Args) > 1 {
+			name = os.Args[1]
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.GetMessage())
+		cancel()
+		conn.Close()
+		time.Sleep(time.Duration(interval) * time.Millisecond)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
 }
